4e033f06714aee0ef5268847ee701f6fa8cb7a05
ji10bhatt <jjb3fh@virginia.edu>
2016-06-05 16:06:32 +0200

    function analyze()

---
+++ R/mainscript.r
@@ -0,0 +1,53 @@
+#install.packages("tm.lexicon.GeneralInquirer", repos="http://datacube.wu.ac.at", type="source")
+library(tm)
+library(tm.lexicon.GeneralInquirer)
+library(rplos)
+library(SnowballC)
+library(RedditExtractoR)
+analyze <- function(x) {
+
+  test <- reddit_content(x)
+  comments <- test$comment
+  head(comments)
+  corpus <- Corpus(VectorSource(comments))
+
+  as.character(corpus[[8]]) #original with no transformations
+
+  corpus <- tm_map(corpus, content_transformer(tolower), lazy=T) #lower case conversion
+  as.character(corpus[[8]]) #checked -- works
+
+  corpus <- tm_map(corpus, removePunctuation, lazy=T)
+  as.character(corpus[[8]])
+
+  corpus <- tm_map(corpus, removeNumbers, lazy=T) #remove numbers
+  as.character(corpus[[8]])
+
+  corpus <- tm_map(corpus, removeWords, stopwords("english"), lazy=T) #remove stop words
+  as.character(corpus[[8]])
+
+  corpus <- tm_map(corpus, stripWhitespace, lazy=T) #strip whitespace
+  as.character(corpus[[8]])
+
+  corpus <- tm_map(corpus, stemDocument, lazy=T)
+  as.character(corpus[[8]])
+
+  dtm <- DocumentTermMatrix(corpus) #assign to a dtm variable
+  as.matrix(dtm)
+
+  class(corpus)
+  class(corpus[1])
+  class(corpus[[1]])
+
+  positive <- sapply(corpus, tm_term_score, terms_in_General_Inquirer_categories("Positiv"))
+  negative <- sapply(corpus, tm_term_score, terms_in_General_Inquirer_categories("Negativ"))
+
+  margin <- positive - negative # negative score means more negative than positive
+
+  print(mean(margin))
+  print(sum(margin))
+}
+# load a sample thread
+
+analyze("https://www.reddit.com/r/worstof/comments/tk3cn/redditor_is_a_complete_asshole_and_demands_an/")
+analyze("https://www.reddit.com/r/UpliftingNews/comments/4mjty0/norway_becomes_first_country_in_the_world_to/")
+
